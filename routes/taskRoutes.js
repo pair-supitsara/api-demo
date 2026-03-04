8761809c29e4e1b1977730cb08fd3c188cb0e349
Supisara Deeto <Supisara.Dee@silkspan.local>
2026-03-04 13:37:05 +0700

    add new document tasks

---
+++ routes/taskRoutes.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = express.Router();
+const Task = require("../models/Task");
+
+// CREATE
+router.post("/", async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    const savedTask = await task.save();
+    res.json(savedTask);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// READ ALL
+router.get("/", async (req, res) => {
+  try {
+    const task = await Task.find();
+    res.json(task);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// READ ONE
+router.get("/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    res.json(task);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// UPDATE
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(updatedTask);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({ message: "Deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 // routes
 app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/tasks", require("./routes/taskRoutes"));
 
 const PORT = process.env.PORT || 5000;
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
